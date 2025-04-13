1d2bcf1597c87f399b080a5098836858878926c5
Diellza <diellzahyseni2003@gmail.com>
2025-04-13 19:45:19 +0200

    Folders structured

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql",
+  uri: "https://rickandmortyapi.com/graphql", //env me qit
   cache: new InMemoryCache(),
 });
 
